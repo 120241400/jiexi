1ffe177717753070d2dad2c115a6fb2f021e40c2
120241400 <89237995+120241400@users.noreply.github.com>
2022-10-20 20:47:41 +0800

    Update kouling.js

--- muiplayer/kouling.js
+++ muiplayer/kouling.js
@@ -158,7 +158,7 @@
 		//	if(!koukou){    
 			var kou = new Array();
 			//ry
-	      	kou[1] = "##VhaEOUpNsw8##";
+	      	kou[1] = "##l5wr8Pnu5FbFw8##";
 			//hw9
 			var min = 1;
             var max = 5;
